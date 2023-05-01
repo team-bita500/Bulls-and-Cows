5bfc38d55088b8b01a6092bf0544bdac2b223838
eunjin917 <cgoya01@likelion.org>
2023-05-01 20:40:55 +0900

    feat: print 'bulls'

--- count_bulls_cows.py
+++ count_bulls_cows.py
@@ -1,4 +1,18 @@
-def count_bulls_cows(user_input):
+def count_bulls_cows(answer, user_input):
+
+
     bulls = 0
     cows = 0
+
+    for i in range(4):
+        if answer[i] == user_input[i]:
+            bulls += 1
+
+    print(bulls)
+    print(cows)
+            
     return bulls, cows
+
+
+## main
+count_bulls_cows('1564', '1634')
